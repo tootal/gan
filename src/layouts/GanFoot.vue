6a81ba252b9fa43b59fcfe5354ff0cce1b26683a
tootal <tootal@yeah.net>
2020-06-17 07:59:43 +0800

    添加网页底部

---
+++ src/layouts/GanFoot.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="text-center">
+    <p>©2020 章又贡 版权所有</p>
+    <p>本站图文内容版权属于原创作者，未经许可不得转载</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "GanFooter"
+};
+</script>
--- src/layouts/GanMain.vue
+++ src/layouts/GanMain.vue
@@ -1,9 +1,20 @@
 <template>
   <div>
     <el-container>
-      <el-header height="50px" class="border"></el-header>
+      <el-header height="50px"></el-header>
       <el-main class="border border-primary"></el-main>
-      <el-footer height="90px" class="border"></el-footer>
+      <el-footer height="90px">
+        <gan-foot></gan-foot>
+      </el-footer>
     </el-container>
   </div>
 </template>
+<script>
+import GanFoot from './GanFoot.vue'
+export default {
+  name: 'GanMain',
+  components: {
+    GanFoot,
+  }
+}
+</script>
