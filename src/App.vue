93f8fc359bc7ddfd14626a1f541faa7be7bc8763
tootal <tootal@yeah.net>
2020-06-16 23:45:58 +0800

    引入element-ui

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <template>
   <div id="app">
     <p>这里是首页</p>
+    <el-button>测试Element按钮</el-button>
   </div>
 </template>
 
 <script>
+import Vue from 'vue'
+import { Button } from 'element-ui'
+Vue.use(Button)
 export default {
   name: 'App',
 }
