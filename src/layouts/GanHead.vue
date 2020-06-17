2ece33867a05b62dd06c1f4f8642f080148efb0b
tootal <tootal@yeah.net>
2020-06-17 09:12:45 +0800

    添加登录以及注册按钮

--- src/layouts/GanHead.vue
+++ src/layouts/GanHead.vue
@@ -1,14 +1,17 @@
 <template>
   <el-row type="flex" align="middle" class="h-100">
-    <el-col :span="2" :xs="4" class="h-100">
+    <el-col :span="2" :sm="4" :xs="4" class="h-100">
       <el-image :src="require('../assets/danboard.png')" 
         alt="Danboard" fit="contain" class="h-100"></el-image>
     </el-col>
-    <el-col :span="18" :xs="16" class="border">
+    <el-col :span="18" :sm="12" :xs="6" class="border">
       <div>中间</div>
     </el-col>
-    <el-col :span="4">
-      <div>右边</div>
+    <el-col :span="4" :sm="8" :xs="14" class="text-center">
+      <el-button-group>
+        <el-button>登录</el-button>
+        <el-button type="primary" plain>注册</el-button>
+      </el-button-group>
     </el-col>
   </el-row>
 </template>
