0038224613cf646f15aea92b3e48a9777d12b3be
tootal <tootal@yeah.net>
2020-06-17 08:54:07 +0800

    添加头部logo

--- src/layouts/GanHead.vue
+++ src/layouts/GanHead.vue
@@ -1,14 +1,19 @@
 <template>
-  <div>
-    <el-row>
-      <el-col :span="24">
-        <div class="border border-primary">这里是网页头部</div>
-      </el-col>
-    </el-row>
-  </div>
+  <el-row type="flex" align="middle" class="h-100">
+    <el-col :span="2" :xs="4" class="h-100">
+      <el-image :src="require('../assets/danboard.png')" 
+        alt="Danboard" fit="contain" class="h-100"></el-image>
+    </el-col>
+    <el-col :span="18" :xs="16" class="border">
+      <div>中间</div>
+    </el-col>
+    <el-col :span="4">
+      <div>右边</div>
+    </el-col>
+  </el-row>
 </template>
 <script>
 export default {
-  name: 'GanHead',
-}
+  name: "GanHead"
+};
 </script>
--- src/layouts/GanMain.vue
+++ src/layouts/GanMain.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
     <el-container>
-      <el-header height="50px"></el-header>
+      <el-header height="50px">
+        <gan-head></gan-head>
+      </el-header>
       <el-main class="border border-primary" style="padding-bottom: 500px"></el-main>
       <el-footer height="90px">
         <gan-foot></gan-foot>
@@ -10,10 +12,12 @@
   </div>
 </template>
 <script>
+import GanHead from './GanHead.vue'
 import GanFoot from './GanFoot.vue'
 export default {
   name: 'GanMain',
   components: {
+    GanHead,
     GanFoot,
   }
 }
