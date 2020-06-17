fcdad78b1f054a88ac89476098a2d21ad68eebbc
tootal <tootal@yeah.net>
2020-06-17 23:01:47 +0800

    添加论坛表格

--- src/pages/GanForum.vue
+++ src/pages/GanForum.vue
@@ -1,21 +1,36 @@
 <template>
   <div>
-    <p>这里是论坛</p>
-    <p>测试文字</p>
-    <p>测试文字</p>
-    <p>测试文字</p>
-    <p>测试文字</p>
+      <el-table :data="tableData" class="forum-table">
+    <el-table-column prop="topic" label="主题"></el-table-column>
+    <el-table-column prop="author" label="作者" width="180"></el-table-column>
+    <el-table-column prop="reply" label="回复" width="180"></el-table-column>
+  </el-table>
   </div>
 </template>
 <script>
+import {StatementGenerator, QuestionGenerator} from '../utils/QuestionGenerator.js';
+import {ChineseNameGenerator} from '../utils/ChineseNameGenerator.js';
 export default {
-  name: "GanForum"
+  name: "GanForum",
+  computed: {
+    tableData: function() {
+      let d = [];
+      for (let i = 0; i < 3 + Math.floor(60 * Math.random()); i++) {
+        let o = {};
+        o.statement = StatementGenerator();
+        o.topic = QuestionGenerator(o.statement);
+        o.author = ChineseNameGenerator();
+        o.reply = Math.floor(50 * Math.random());
+        d.push(o);
+      }
+      return d;
+    }
+  }
 };
 </script>
 <style scoped>
-p {
-  text-align: center;
-  margin-top: 400px;
-  margin-bottom: 400px;
+.forum-table {
+  max-width: 1024px;
+  margin: 20px auto;
 }
 </style>
