e35493a0109116745cb7d63e290da8d96ab792a4
tootal <tootal@yeah.net>
2020-06-18 10:49:25 +0800

    添加论坛文章

--- src/pages/GanPost.vue
+++ src/pages/GanPost.vue
@@ -1,13 +1,27 @@
 <template>
-  <p class="text-center">{{ post.topic }}</p>
+  <div>
+    <p class="text-center">{{ post.topic }}</p>
+    <p class="article" v-for="(o, i) in article" :key="i"> {{ i + o }} </p>
+    <p class="article">{{ article }}</p>
+  </div>
 </template>
 <script>
+import BullshitGenerator from "../utils/BullshitGenerator.js";
 export default {
   name: "GanPost",
   computed: {
     post() {
       return this.$root.forumData[this.$route.params.id - 1];
+    },
+    article() {
+      return BullshitGenerator(this.post.statement);
     }
   }
 };
 </script>
+<style scoped>
+.article {
+  max-width: 1024px;
+  margin: 10px auto;
+}
+</style>
