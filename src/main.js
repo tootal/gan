d05cda43b47cc846be9e972b66a50959c9a3a68a
tootal <tootal@yeah.net>
2020-06-18 14:55:22 +0800

    分离路由配置

--- src/main.js
+++ src/main.js
@@ -6,12 +6,7 @@ import 'element-ui/lib/theme-chalk/index.css'
 import App from './App.vue'
 import './styles/gan.scss'
 
-import GanIndex from './pages/GanIndex.vue'
-import GanForum from './pages/GanForum.vue'
-import GanLocation from './pages/GanLocation.vue'
-import GanError from './pages/GanError.vue'
-import GanDetail from './pages/GanDetail.vue'
-import GanPost from './pages/GanPost.vue'
+import routes from './routes'
 
 Vue.use(VueRouter)
 Vue.use(BaiduMap, {
@@ -36,39 +31,7 @@ const router = new VueRouter({
       }
     }
   },
-  routes: [
-    {
-      path: '/index',
-      component: GanIndex,
-      name: 'index',
-      alias: '/'
-    },
-    {
-      path: '/forum',
-      component: GanForum,
-      name: 'forum'
-    },
-    {
-      path: '/forum/:id',
-      component: GanDetail,
-      name: 'detail',
-    },
-    {
-      path: '/post',
-      component: GanPost,
-      name: 'post'
-    },
-    {
-      path: '/location',
-      component: GanLocation,
-      name: 'location'
-    },
-    {
-      path: '*',
-      component: GanError,
-      name: 'error'
-    }
-  ]
+  routes
 })
 new Vue({
   el: '#app',
---
+++ src/routes.js
@@ -0,0 +1,42 @@
+import GanIndex from './pages/GanIndex.vue'
+import GanForum from './pages/GanForum.vue'
+import GanLocation from './pages/GanLocation.vue'
+import GanError from './pages/GanError.vue'
+import GanDetail from './pages/GanDetail.vue'
+import GanPost from './pages/GanPost.vue'
+
+let routes = [
+  {
+    path: '/index',
+    component: GanIndex,
+    name: 'index',
+    alias: '/'
+  },
+  {
+    path: '/forum',
+    component: GanForum,
+    name: 'forum'
+  },
+  {
+    path: '/forum/:id',
+    component: GanDetail,
+    name: 'detail',
+  },
+  {
+    path: '/post',
+    component: GanPost,
+    name: 'post'
+  },
+  {
+    path: '/location',
+    component: GanLocation,
+    name: 'location'
+  },
+  {
+    path: '*',
+    component: GanError,
+    name: 'error'
+  }
+]
+
+export default routes;
