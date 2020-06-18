2fcbb989aaac0ac0f85c60bd9e0254aa917800bf
tootal <tootal@yeah.net>
2020-06-19 05:41:45 +0800

    添加Git记录文件

--- scripts/gitlog.js
+++ scripts/gitlog.js
@@ -1,6 +1,6 @@
 var process = require('child_process');
 
-var cmd = 'git log --no-merges --date=format:"%Y/%m/%d-%H:%M:%S" --abbrev-commit --pretty=format:"%h %cd %s"';
+var cmd = 'git log --no-merges --date=format:"%Y/%m/%d-%H:%M:%S" --pretty=format:"%H %cd %s"';
 process.exec(cmd, function(error, stdout, stderr) {
   console.log(stdout);
 });
