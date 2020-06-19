18750cf4ac3d823d7b1cdbec43416669b9af7ce9
tootal <tootal@yeah.net>
2020-06-19 08:45:46 +0800

    添加注册表单

--- src/pages/GanRegister.vue
+++ src/pages/GanRegister.vue
@@ -1,12 +1,31 @@
 <template>
-  <gan-content>
+  <gan-content :content-style="{ padding: '60px 0' }">
     <h1 class="text-center">注册</h1>
+    <el-form ref="form" :model="form" label-width="80px">
+      <el-form-item label="用户名">
+        <el-input v-model="form.user" v-focus></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input v-model="form.password"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="handlePost">注册</el-button>
+      </el-form-item>
+    </el-form>
   </gan-content>
 </template>
 <script>
 import GanContent from "../layouts/GanContent.vue";
 export default {
   name: "GanRegister",
+  data() {
+    return {
+      form: {
+        user: "",
+        password: "",
+      }
+    };
+  },
   components: {
     GanContent
   }
