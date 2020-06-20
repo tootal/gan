3371ee8da885b70b9efd0a6d06c2dfae59ee0cb5
tootal <tootal@yeah.net>
2020-06-20 17:51:11 +0800

    略微美化了一下用户名界面

--- src/pages/GanUser.vue
+++ src/pages/GanUser.vue
@@ -1,7 +1,12 @@
 <template>
   <div>
-    <gan-content>
-      用户{{ $route.params.id }}
+    <gan-content max-width="200px" content-padding="95px 0" card-padding="120px 0">
+      <div class="text-center">
+        <div>
+          <el-avatar icon="el-icon-user-solid"></el-avatar>
+        </div>
+        <div class="user-name">{{ $route.params.id }}</div>
+      </div>
     </gan-content>
   </div>
 </template>
@@ -11,6 +16,11 @@ export default {
   name: "GanUser",
   components: {
     GanContent
-  },
+  }
 };
 </script>
+<style scoped>
+.user-name {
+  font-weight: bold;
+}
+</style>
