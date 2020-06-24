1e803dcaf59dbe8c250224a8c0b8b1fb2fe597f0
tootal <tootal@yeah.net>
2020-06-24 19:35:09 +0800

    更新gitlog脚本

--- scripts/gitlog.js
+++ scripts/gitlog.js
@@ -1,6 +1,6 @@
 var process = require('child_process');
-
+var fs = require('fs')
 var cmd = 'git log --no-merges --date=format:"%Y/%m/%d-%H:%M:%S" --pretty=format:"%H %cd %s"';
-process.exec(cmd, function(error, stdout, stderr) {
-  console.log(stdout);
+process.exec(cmd, function(error, stdout) {
+  fs.writeFile('gitlog.txt', stdout, ()=>{});
 });
